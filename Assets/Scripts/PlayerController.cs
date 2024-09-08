42d1be2c39b525b95bdf0680b4aaa90a07bc7eca
ただの化け物 <masakingu008@gmail.com>
2024-09-09 02:43:23 +0900

    main/BasicAction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,35 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject Camera;
     [SerializeField] private GameObject PlayerSpriteObject;
     [SerializeField] private InputActionAsset inputActions;
-    public InputAction moveAction;
+    
+    private InputAction moveAction;
+    private bool isGround;
     private Vector2 moveInput;
     private Rigidbody rigidbody;
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;  // 移動速度
+    public float jumpForce = 130f;  // ジャンプ力
+
     void Start()
     {
+        // アクションマップとアクションの初期化
         var playerActionMap = inputActions.FindActionMap("Player");
         rigidbody = this.GetComponent<Rigidbody>();
         moveAction = playerActionMap.FindAction("Move");
+
+        // アクションを有効化
+        moveAction.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // キャラクターの向きをカメラに常に向ける
         PlayerSpriteObject.transform.LookAt(Camera.transform.position);
-        moveInput = moveAction.ReadValue<Vector2>();
-        // 取得したVector2の値でキャラクターを移動
-        Vector3 movement = new Vector3(moveInput.x*5f, 0,0 );
-        transform.Translate(movement * Time.deltaTime);
-        rigidbody.AddForce(new Vector3(0f,moveInput.y*98f,0f));
-    }
-    private void FixedUpdate(){
 
+        // 入力の取得
+        moveInput = moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        // 水平方向の移動
+        Vector3 movement = new Vector3(moveInput.x * moveSpeed, rigidbody.velocity.y, 0);
+        rigidbody.velocity = movement;
+
+        // ジャンプ処理
+        if (isGround && moveInput.y > 0)
+        {
+            // 一度だけジャンプ力を適用する
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGround = false;  // ジャンプ中は地面についていない
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 接地判定: 地面に触れたときのみジャンプが可能
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = true;  // 接地したら再びジャンプ可能に
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // 地面から離れたときはジャンプ不可にする
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = false;
+        }
     }
 }
