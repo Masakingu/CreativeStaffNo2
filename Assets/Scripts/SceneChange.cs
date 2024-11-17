4834669c599f78f314010a3257400639dcc5ea20
ただの化け物 <masakingu008@gmail.com>
2024-11-17 20:33:17 +0900

    main/Add_ClickSE

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -8,7 +8,10 @@ public class SceneChange : MonoBehaviour
 {
     
     [SerializeField] private string SceneName;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip ClickSE;
     public void OnClick(){
+        audioSource.PlayOneShot(ClickSE);
         SceneManager.LoadScene(SceneName);
     }
 }
